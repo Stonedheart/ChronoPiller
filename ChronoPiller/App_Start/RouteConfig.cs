3917eb9e5f27443f2fe2c3269be517228fcc8c9d
Stonedheart <tytomaszto@gmail.com>
2018-01-14 17:01:05 +0100

    Define new route

--- ChronoPiller/App_Start/RouteConfig.cs
+++ ChronoPiller/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace ChronoPiller
@@ -17,6 +13,12 @@ namespace ChronoPiller
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "PrescriptionDetails",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "PrescriptionDetails", id = UrlParameter.Optional }
             );
         }
     }
