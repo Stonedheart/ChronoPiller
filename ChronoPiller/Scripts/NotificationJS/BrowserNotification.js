81f1efc8c3439b459e7d229e5b396117aa52951a
Lukasz Bielenin <l.bielenin@gmail.com>
2018-01-14 17:52:47 +0100

    Implement test browser reminder.

--- ChronoPiller/Scripts/NotificationJS/BrowserNotification.js
+++ ChronoPiller/Scripts/NotificationJS/BrowserNotification.js
@@ -1,6 +1,23 @@
-function ready() { $(function () {
-    Notification.requestPermission()
-    
-})
-    
+var options = {
+    body: "Surf on, nigga!",
+    icon: 'http://i0.kym-cdn.com/photos/images/newsfeed/001/104/774/107.jpg'
+};
+
+function ready() {
+    $(function () {
+        if (Notification.permission == 'granted') {
+
+            var hello = new Notification('Hello!', options);
+        }
+        else {
+            Notification.requestPermission().then(function (result) {
+                if (result == 'granted') {
+                    var grantedNote = new Notification("Permission granted!", options);
+                }
+
+            })
+        }
+
+    })
+
 }
