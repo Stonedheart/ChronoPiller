908c1472339622afff829df658238d016a8001d0
Lukasz Bielenin <l.bielenin@gmail.com>
2018-01-14 21:45:42 +0100

    Refactor requestpermission js function.

--- ChronoPiller/Scripts/NotificationJS/BrowserNotification.js
+++ ChronoPiller/Scripts/NotificationJS/BrowserNotification.js
@@ -3,21 +3,15 @@ var options = {
     icon: 'http://i0.kym-cdn.com/photos/images/newsfeed/001/104/774/107.jpg'
 };
 
-function ready() {
+function checkForPermission() {
     $(function () {
-        if (Notification.permission == 'granted') {
+        if (Notification.permission == !'granted') {
 
-            var hello = new Notification('Hello!', options);
-        }
-        else {
             Notification.requestPermission().then(function (result) {
                 if (result == 'granted') {
                     var grantedNote = new Notification("Permission granted!", options);
                 }
-
             })
         }
-
     })
-
 }
