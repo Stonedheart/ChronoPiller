0ce93239f393e5343b71a3c9258e2469bd83dd34
Stonedheart <tytomaszto@gmail.com>
2018-01-14 15:10:20 +0100

    Add Medicine class

---
+++ ChronoPiller/Models/Medicine.cs
@@ -0,0 +1,14 @@
+﻿namespace ChronoPiller.Models
+{
+    public class Medicine
+    {
+        public string Name { get; }
+        public int Interval { get; }
+
+        public Medicine(string name, int interval)
+        {
+            Name = name;
+            Interval = interval;
+        }
+    }
+}
