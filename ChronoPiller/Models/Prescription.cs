e7c78b91c5313dae50294cee2b4081b9a1e7228c
Stonedheart <tytomaszto@gmail.com>
2018-01-14 15:02:05 +0100

    Add Prescription class

---
+++ ChronoPiller/Models/Prescription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChronoPiller.Models
+{
+    public class Prescription
+    {
+        public string Name { get; }
+
+        public Prescription(string name)
+        {
+            Name = name;
+        }
+    }
+}
