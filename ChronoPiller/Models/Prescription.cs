a9df4793ce7ea46e8813606ae1e1bec40b0f66fd
Stonedheart <tytomaszto@gmail.com>
2018-01-14 15:10:33 +0100

    Add field to Prescription class

--- ChronoPiller/Models/Prescription.cs
+++ ChronoPiller/Models/Prescription.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
 namespace ChronoPiller.Models
 {
@@ -6,6 +9,7 @@ namespace ChronoPiller.Models
     {
         public string Name { get; }
         public DateTime StartUseDate { get; }
+        public List<Medicine> Medicines = new List<Medicine>();
 
         public Prescription(string name, DateTime startUseDate)
         {
