bc773d3796abcb9c7113b9cac3b8b1e7ecfa6953
Lukasz Bielenin <l.bielenin@gmail.com>
2018-01-15 17:56:17 +0100

    Fix non working script.

--- ChronoPiller/Scripts/NotificationJS/BrowserNotification.js
+++ ChronoPiller/Scripts/NotificationJS/BrowserNotification.js
@@ -1,17 +1,28 @@
+var d = new Date();
+
 var options = {
-    body: "Surf on, nigga!",
+    body: "Surf on, nigga! " + d.toDateString(),
     icon: 'http://i0.kym-cdn.com/photos/images/newsfeed/001/104/774/107.jpg'
 };
 
+
 function checkForPermission() {
     $(function () {
-        if (Notification.permission == !'granted') {
-
-            Notification.requestPermission().then(function (result) {
-                if (result == 'granted') {
-                    var grantedNote = new Notification("Permission granted!", options);
-                }
-            })
+        var helloNote = new Notification("Already granted!");
+        if (Notification.permission === "granted") {
+            // If it's okay let's create a notification
+            helloNote.show();
         }
+
+        Notification.requestPermission().then(function (result) {
+            if (result === 'granted') {
+                var grantedNote = new Notification("Granted Now", options);
+            }
+            else {
+
+                console.log("Failed")
+            }
+        })
     })
+
 }
