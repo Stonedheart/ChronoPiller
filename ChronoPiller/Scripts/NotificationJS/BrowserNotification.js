bc2f7f3ca848a717d1e0d5fe057b72f2e1c71a00
Lukasz Bielenin <l.bielenin@gmail.com>
2018-01-16 16:37:26 +0100

    Made prototype with button.

--- ChronoPiller/Scripts/NotificationJS/BrowserNotification.js
+++ ChronoPiller/Scripts/NotificationJS/BrowserNotification.js
@@ -1,28 +1,41 @@
-var d = new Date();
-
 var options = {
-    body: "Surf on, nigga! " + d.toDateString(),
+    body: "Surf on, nigga!\n",
     icon: 'http://i0.kym-cdn.com/photos/images/newsfeed/001/104/774/107.jpg'
 };
 
 
 function checkForPermission() {
     $(function () {
-        var helloNote = new Notification("Already granted!");
-        if (Notification.permission === "granted") {
-            // If it's okay let's create a notification
-            helloNote.show();
+        console.log(Notification.permission);
+        if (Notification.permission === 'granted') {
+            var helloNote = new Notification('Already granted!');
+
+        } else {
+
+            Notification.requestPermission().then(function (result) {
+                if (result === 'granted') {
+                    var grantedNote = new Notification("Granted Now", options);
+                }
+                else {
+
+                    console.log("Failed")
+                }
+            })
         }
+    })
 
-        Notification.requestPermission().then(function (result) {
-            if (result === 'granted') {
-                var grantedNote = new Notification("Granted Now", options);
-            }
-            else {
+}
 
-                console.log("Failed")
-            }
+function remind() {
+    console.log("I see function!");
+    $(function () {
+        $("#remind").click(function () {
+            console.log("I see button!");
+            var reminderNote = new Notification("Take your pill!", options);
+
         })
+
+
     })
 
 }
