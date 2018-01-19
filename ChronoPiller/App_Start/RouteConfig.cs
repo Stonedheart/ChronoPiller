4addc0cd3b81a32184f208265db2ce403684f8bc
Stonedheart <tytomaszto@gmail.com>
2018-01-19 12:03:14 +0100

    Remove unnecessary route

--- ChronoPiller/App_Start/RouteConfig.cs
+++ ChronoPiller/App_Start/RouteConfig.cs
@@ -8,17 +8,11 @@ namespace ChronoPiller
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
-
-            routes.MapRoute(
-                name: "PrescriptionDetails",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "PrescriptionDetails", id = UrlParameter.Optional }
             );
         }
     }
