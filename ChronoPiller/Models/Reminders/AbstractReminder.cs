17f644cec9880d49b36f4fb63983d0844ae35425
Lukasz Bielenin <l.bielenin@gmail.com>
2018-01-24 19:45:31 +0100

    Implement minor changes to abstractReminder

--- ChronoPiller/Models/Reminders/AbstractReminder.cs
+++ ChronoPiller/Models/Reminders/AbstractReminder.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 
 namespace ChronoPiller.Models.Reminders
 {
-    public class AbstractReminder
+    public abstract class AbstractReminder
     {
         public Dictionary<string, int> PillsQuantity;
         public DateTime Time;
+
+        public abstract void Send();
 
     }
 }
