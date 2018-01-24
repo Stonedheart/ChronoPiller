a4158c2a1dbb3ff6f2de40920867391a312d22af
Lukasz Bielenin <l.bielenin@gmail.com>
2018-01-24 19:44:01 +0100

    Add AbstractReminder class

---
+++ ChronoPiller/Models/Reminders/AbstractReminder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ChronoPiller.Models.Reminders
+{
+    public class AbstractReminder
+    {
+        public Dictionary<string, int> PillsQuantity;
+        public DateTime Time;
+
+    }
+}
