ed7c473237a8129eca7675cd05a4a32c7475b000
Stonedheart <tytomaszto@gmail.com>
2018-01-26 15:37:16 +0100

    Add Index method in HomeController

--- ChronoPiller/Controllers/HomeController.cs
+++ ChronoPiller/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using ChronoPiller.DAL;
 using ChronoPiller.Models;
 
 namespace ChronoPiller.Controllers
@@ -9,73 +8,12 @@ namespace ChronoPiller.Controllers
     {
         public ActionResult Index()
         {
-            if (Session["prescriptions"] == null)
-            {
-                Session["prescriptions"] = new List<Prescription>();
-            }
+            var dbContext = new ChronoPillerDB();
+            var user = new User("TestUser", "1234");
+            dbContext.Users.Add(user);
+            dbContext.SaveChanges();
 
-            return View((List<Prescription>)Session["prescriptions"]);
-        }
-
-        [HttpGet]
-        public ActionResult AddPrescription()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ActionResult AddPrescription(FormCollection form)
-        {
-            var name = form["name"];
-            var prescription = new Prescription(name);
-
-            var prescriptions = (List<Prescription>) Session["prescriptions"];
-            prescriptions.Add(prescription);
-
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet]
-        public ActionResult PrescriptionDetails(string id)
-        {
-            var prescriptions = (List<Prescription>)Session["prescriptions"];
-
-            foreach (var prescription in prescriptions)
-            {
-                if (Equals(prescription.Name, id))
-                {
-                    return View(prescription);
-                }
-            }
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet]
-        public ActionResult MedicineDetails(string id)
-        {
-            return View("MedicineDetails", (object)id);
-        }
-
-        [HttpPost]
-        public ActionResult MedicineDetails(FormCollection form)
-        {
-            var name = form["name"];
-            var startUseDate = form["startUseDate"];
-            var interval = form["interval"];
-            var prescriptionId = form["prescriptionId"];
-
-            var medicine = new Medicine(name, DateTime.Parse(startUseDate), int.Parse(interval));
-            var prescriptions = (List<Prescription>) Session["prescriptions"];
-
-            foreach (var prescription in prescriptions)
-            {
-                if (prescription.Name == prescriptionId)
-                {
-                    prescription.Medicines.Add(medicine);
-                }
-            }
-
-            return RedirectToAction("PrescriptionDetails", "Home", new { id = prescriptionId });
+            return View(user);
         }
     }
 }
