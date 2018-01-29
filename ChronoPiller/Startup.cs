b77514c036a78fc6608c2db66c31a05932295cd2
Lukasz Bielenin <l.bielenin@gmail.com>
2018-01-29 07:58:25 +0100

    Add startup class

---
+++ ChronoPiller/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Hangfire;
+using Hangfire.SqlServer;
+using Owin;
+
+namespace ChronoPiller
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            GlobalConfiguration.Configuration.UseSqlServerStorage("MailerDb", new SqlServerStorageOptions()
+            {
+                QueuePollInterval = TimeSpan.FromSeconds(1)
+            });
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
