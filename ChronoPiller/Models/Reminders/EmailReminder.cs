93d728e85259da01c87b049e057310c89a5ddc64
Lukasz Bielenin <l.bielenin@gmail.com>
2018-01-29 08:21:36 +0100

    Remodell email template

--- ChronoPiller/Models/Reminders/EmailReminder.cs
+++ ChronoPiller/Models/Reminders/EmailReminder.cs
@@ -4,15 +4,15 @@ namespace ChronoPiller.Models.Reminders
 {
     public class EmailReminder : Email
     {
-        public EmailReminder(string viewName, string to, string userName, string comment) : base(viewName)
+        public EmailReminder(string viewName, string to, string name) : base(viewName)
         {
             To = to;
-            UserName = userName;
-            Comment = comment;
+            Name = name;
         }
 
+        public string Name { get; set; }
+
         public string To { get; set; }
-        public string UserName { get; set; }
         public string Comment { get; set; }
     }
 }
