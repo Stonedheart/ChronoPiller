e24613c376030f2979599fd1bf095a0840379d59
Lukasz Bielenin <l.bielenin@gmail.com>
2018-01-29 08:11:31 +0100

    Add email template and email class

---
+++ ChronoPiller/Models/Reminders/EmailReminder.cs
@@ -0,0 +1,18 @@
+﻿using Postal;
+
+namespace ChronoPiller.Models.Reminders
+{
+    public class EmailReminder : Email
+    {
+        public EmailReminder(string viewName, string to, string userName, string comment) : base(viewName)
+        {
+            To = to;
+            UserName = userName;
+            Comment = comment;
+        }
+
+        public string To { get; set; }
+        public string UserName { get; set; }
+        public string Comment { get; set; }
+    }
+}
