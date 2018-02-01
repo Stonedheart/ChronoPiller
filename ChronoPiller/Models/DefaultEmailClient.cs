d2fb46a81d9c0e21cb763180670d54ed5bd79e04
Lukasz Bielenin <l.bielenin@gmail.com>
2018-02-01 12:06:22 +0100

    Modify DefaultEmailClient class

---
+++ ChronoPiller/Models/DefaultEmailClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace ChronoPiller.Models
+{
+    public class DefaultEmailClient : SmtpClient
+    {
+        private const string Host = "smtp.gmail.com";
+        private const int Port = 587;
+        private const bool EnableSsl = true;
+        public int Timeout = 20000;
+        private const SmtpDeliveryMethod DeliveryMethod = SmtpDeliveryMethod.Network;
+        private const bool UseDefaultCredentials = false;
+        public NetworkCredential Credentials;
+
+        public DefaultEmailClient(string defaultName, string defaultPassword)
+        {
+            Credentials = new NetworkCredential(defaultName, defaultPassword);
+        }
+    }
+}
