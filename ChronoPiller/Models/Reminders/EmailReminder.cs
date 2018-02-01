1e324b4428192572576e0b917f65ad99e6bc11fc
Lukasz Bielenin <l.bielenin@gmail.com>
2018-02-01 11:43:50 +0100

    Implement constructor for EmailReminder class

--- ChronoPiller/Models/Reminders/EmailReminder.cs
+++ ChronoPiller/Models/Reminders/EmailReminder.cs
@@ -1,11 +1,16 @@
-﻿using Newtonsoft.Json;
+﻿using System.Net.Mail;
+using Newtonsoft.Json;
 
 namespace ChronoPiller.Models.Reminders
 {
-    public class EmailReminder
+    public class EmailReminder : MailMessage
     {
-        public string Name { get; set; }
+        public string ReceiverName { get; set; }
 
-        public string To { get; set; }
+        public EmailReminder(string @from, string to, 
+            string subject, string body, string name) : base(@from, to, subject, body)
+        {
+            ReceiverName = name;
+        }
     }
 }
