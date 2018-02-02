be529406a134a5db45923da7f5591a665f9a4909
Lukasz Bielenin <l.bielenin@gmail.com>
2018-02-02 15:22:25 +0100

    Remodel email reminder for factory method

--- ChronoPiller/Models/Reminders/EmailReminder.cs
+++ ChronoPiller/Models/Reminders/EmailReminder.cs
@@ -4,12 +4,17 @@ namespace ChronoPiller.Models.Reminders
 {
     public class EmailReminder : MailMessage
     {
-        public Prescription Prescription { get; set; }
-
-        public EmailReminder(string @from, string to, 
-            string subject, string body, Prescription prescription) : base(@from, to, subject, body)
+        public static MailMessage GetEmailReminder(Prescription prescription)
         {
-            Prescription = prescription;
+            var email = new MailMessage("chrono@piller.com", "chronopiller@gmail.com");
+            email.Subject = "Take your daily dose!";
+            email.Body = $"Hello there!\n\n" +
+                         $"Your friendly neighbourhood ChronoPiller would like to remind you about " +
+                         $"your daily dose of {prescription.PrescriptedMedicines} from prescription {prescription.Name}!\n\n" +
+                         $"Take it or You'll be sorry!\n\n" +
+                         $"Cheers!";
+
+            return email;
         }
     }
 }
