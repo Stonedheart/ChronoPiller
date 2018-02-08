9164ff8fb342ed31ee285bc245a35f65fae7cdb3
Lukasz Bielenin <l.bielenin@gmail.com>
2018-02-08 15:51:52 +0100

    Modify ChronoPillerDB to fit recent changes

--- ChronoPiller/DAL/ChronoPillerDB.cs
+++ ChronoPiller/DAL/ChronoPillerDB.cs
@@ -1,11 +1,14 @@
 ﻿using System.Data.Entity;
 using ChronoPiller.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace ChronoPiller.DAL
 {
-    public class ChronoPillerDB : DbContext
+    public class ChronoPillerDB : IdentityDbContext<User>
     {
-        public DbSet<User> Users { get; set; }
+        public ChronoPillerDB() : base("ChronoPiller.DAL.ChronoPillerDB")
+        { }
+        public override IDbSet<User> Users { get; set; }
         public DbSet<Prescription> Prescriptions { get; set; }
         public DbSet<PrescriptedMedicine> PrescriptedMedicines { get; set; }
         public DbSet<MedicineBox> MedicineBoxes { get; set; }
