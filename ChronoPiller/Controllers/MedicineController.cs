41877b9d6e7ad6655f78b9a462c5ece57801fe32
Stonedheart <tytomaszto@gmail.com>
2018-02-09 15:04:57 +0100

    Extract getting data from DB to separate methods in MedicineController

--- ChronoPiller/Controllers/MedicineController.cs
+++ ChronoPiller/Controllers/MedicineController.cs
@@ -30,30 +30,67 @@ namespace ChronoPiller.Controllers
             var medicine = new Medicine(name);
             SaveMedToDb(medicine);
 
-            using (ChronoPillerDB dbContext = new ChronoPillerDB())
-            {
-                var medicineId = dbContext.Medicines.FirstOrDefault(x => x.Name == medicine.Name).Id;
-                var medicineBox = new MedicineBox(medicineId, int.Parse(medicineBoxCapacity),
+            var medicineId = GetMedicineId(medicine);
+            var medicineBox = new MedicineBox(medicineId, int.Parse(medicineBoxCapacity),
                     float.Parse(activeSubstanceAmountInMg));
-                dbContext.MedicineBoxes.Add(medicineBox);
-                dbContext.SaveChanges();
+            SaveMedBoxToDb(medicineBox);
 
-                var medicineBoxId = dbContext.MedicineBoxes.FirstOrDefault(x => x.MedicineId == medicineId).Id;
-                var prescriptedMedicine = new PrescriptedMedicine(name, DateTime.Parse(startUsageDate),
+            var medicineBoxId = GetMedicineBoxId(medicineId);
+            var prescriptedMedicine = new PrescriptedMedicine(name, DateTime.Parse(startUsageDate),
                     int.Parse(prescriptedBoxCount), int.Parse(dose), int.Parse(interval), int.Parse(prescriptionId),
                     medicineBoxId);
-                dbContext.PrescriptedMedicines.Add(prescriptedMedicine);
-                dbContext.SaveChanges();
-            }
+            SavePrescriptedMedToDb(prescriptedMedicine);
 
             return RedirectToAction("Details", "Prescription", new { id = int.Parse(prescriptionId) });
         }
 
+        private int GetMedicineBoxId(int medicineId)
+        {
+            int medicineBoxId;
+
+            using (var dbContext = new ChronoPillerDB())
+            {
+                medicineBoxId = dbContext.MedicineBoxes.FirstOrDefault(x => x.MedicineId == medicineId).Id;
+            }
+
+            return medicineBoxId;
+        }
+
+        private int GetMedicineId(Medicine medicine)
+        {
+            int medicineId;
+
+            using (var dbContext = new ChronoPillerDB())
+            {
+                medicineId = dbContext.Medicines.FirstOrDefault(x => x.Name == medicine.Name).Id;
+            }
+
+            return medicineId;
+        }
+
         private void SaveMedToDb(Medicine medicine)
         {
-            using (ChronoPillerDB dbContext = new ChronoPillerDB())
+            using (var dbContext = new ChronoPillerDB())
             {
                 dbContext.Medicines.Add(medicine);
+                dbContext.SaveChanges();
+            }
+        }
+
+        private void SaveMedBoxToDb(MedicineBox medBox)
+        {
+            using (var dbContext = new ChronoPillerDB())
+            {
+                dbContext.MedicineBoxes.Add(medBox);
+                dbContext.SaveChanges();
+            }
+        }
+
+        private void SavePrescriptedMedToDb(PrescriptedMedicine prescriptedMedicine)
+        {
+            using (var dbContext = new ChronoPillerDB())
+            {
+                dbContext.PrescriptedMedicines.Add(prescriptedMedicine);
                 dbContext.SaveChanges();
             }
         }
