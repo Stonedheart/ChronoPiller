c1374a50c35246c62911d07513e196e142ce8e69
Stonedheart <tytomaszto@gmail.com>
2018-02-09 11:13:53 +0100

    Extraxt operations on db to separate methods in MedicineController

--- ChronoPiller/Controllers/MedicineController.cs
+++ ChronoPiller/Controllers/MedicineController.cs
@@ -27,28 +27,35 @@ namespace ChronoPiller.Controllers
             var activeSubstanceAmountInMg = form["activeSubstanceAmountInMg"];
             var medicineBoxCapacity = form["medicineBoxCapacity"];
 
-            var dbContext = new ChronoPillerDB();
-
             var medicine = new Medicine(name);
-            dbContext.Medicines.Add(medicine);
-            dbContext.SaveChanges();
+            SaveMedToDb(medicine);
 
+            using (ChronoPillerDB dbContext = new ChronoPillerDB())
+            {
+                var medicineId = dbContext.Medicines.FirstOrDefault(x => x.Name == medicine.Name).Id;
+                var medicineBox = new MedicineBox(medicineId, int.Parse(medicineBoxCapacity),
+                    float.Parse(activeSubstanceAmountInMg));
+                dbContext.MedicineBoxes.Add(medicineBox);
+                dbContext.SaveChanges();
 
-            var medicineId = dbContext.Medicines.FirstOrDefault(x => x.Name == medicine.Name).Id;
-            var medicineBox = new MedicineBox(medicineId, int.Parse(medicineBoxCapacity),
-                float.Parse(activeSubstanceAmountInMg));
-            dbContext.MedicineBoxes.Add(medicineBox);
-            dbContext.SaveChanges();
-
-            var medicineBoxId = dbContext.MedicineBoxes.FirstOrDefault(x => x.MedicineId == medicineId).Id;
-            var prescriptedMedicine = new PrescriptedMedicine(name, DateTime.Parse(startUsageDate),
-                int.Parse(prescriptedBoxCount), int.Parse(dose), int.Parse(interval), int.Parse(prescriptionId),
-                medicineBoxId);
-            dbContext.PrescriptedMedicines.Add(prescriptedMedicine);
-            dbContext.SaveChanges();
-            dbContext.Dispose();
+                var medicineBoxId = dbContext.MedicineBoxes.FirstOrDefault(x => x.MedicineId == medicineId).Id;
+                var prescriptedMedicine = new PrescriptedMedicine(name, DateTime.Parse(startUsageDate),
+                    int.Parse(prescriptedBoxCount), int.Parse(dose), int.Parse(interval), int.Parse(prescriptionId),
+                    medicineBoxId);
+                dbContext.PrescriptedMedicines.Add(prescriptedMedicine);
+                dbContext.SaveChanges();
+            }
 
             return RedirectToAction("Details", "Prescription", new { id = int.Parse(prescriptionId) });
+        }
+
+        private void SaveMedToDb(Medicine medicine)
+        {
+            using (ChronoPillerDB dbContext = new ChronoPillerDB())
+            {
+                dbContext.Medicines.Add(medicine);
+                dbContext.SaveChanges();
+            }
         }
     }
 }
