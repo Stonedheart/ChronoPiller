56c3ee0f88738d5a13e9a91bd330c69e1777147b
Lukasz Bielenin <l.bielenin@gmail.com>
2018-02-12 14:55:09 +0100

    Implement method for disposing unused references

--- ChronoPiller/Controllers/AccountController.cs
+++ ChronoPiller/Controllers/AccountController.cs
@@ -113,6 +113,26 @@ namespace ChronoPiller.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_userManager != null)
+                {
+                    _userManager.Dispose();
+                    _userManager = null;
+                }
+
+                if (_signInManager != null)
+                {
+                    _signInManager.Dispose();
+                    _signInManager = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+
         private IAuthenticationManager AuthenticationManager
         {
             get
