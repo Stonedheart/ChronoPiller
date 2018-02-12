df44885580e4ff37a6c6284f2676b568709024c5
Lukasz Bielenin <l.bielenin@gmail.com>
2018-02-12 14:12:02 +0100

    Implement view model for registering

--- ChronoPiller/Models/AccountViewModels.cs
+++ ChronoPiller/Models/AccountViewModels.cs
@@ -1,5 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChronoPiller.Models
 {
@@ -11,11 +11,28 @@ namespace ChronoPiller.Models
         public string Email { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name="Password")]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
     }
 
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {1} character long.", MinimumLength = 5)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
 }
