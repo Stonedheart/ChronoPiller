72dd985464634a2ee32c9e5e9deae0f7070c97f2
Lukasz Bielenin <l.bielenin@gmail.com>
2018-02-12 14:05:12 +0100

    Implement Login view model

---
+++ ChronoPiller/Models/AccountViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace ChronoPiller.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name="Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+}
