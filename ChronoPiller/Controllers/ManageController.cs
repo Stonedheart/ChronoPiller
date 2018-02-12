75bd2cddd72720dfdafe8188f12abd3e42dae308
Lukasz Bielenin <l.bielenin@gmail.com>
2018-02-12 15:09:44 +0100

    Implement manage controller with default constructor

---
+++ ChronoPiller/Controllers/ManageController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using ChronoPiller.Models;
+
+namespace ChronoPiller.Controllers
+{
+    [Authorize]
+    public class ManageController : Controller
+    {
+        private ChronoSignInManager _signInManager;
+        private ChronoUserManager _userManager;
+
+        public ManageController() { }
+
+
+    }
+}
