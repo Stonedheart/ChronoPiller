61cedb0b022b5974e0efeed055437768a22ac82c
Lukasz Bielenin <l.bielenin@gmail.com>
2018-02-12 19:18:39 +0100

    Something in accountcontroller

--- ChronoPiller/Controllers/AccountController.cs
+++ ChronoPiller/Controllers/AccountController.cs
@@ -95,7 +95,7 @@ namespace ChronoPiller.Controllers
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
+                    
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
