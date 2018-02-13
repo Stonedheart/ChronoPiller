f7a24ad2d9931e6850ab725a31f5009efaac2693
Stonedheart <tytomaszto@gmail.com>
2018-02-13 16:48:34 +0100

    Get prescritpions list for current user from db in HomeController

--- ChronoPiller/Controllers/HomeController.cs
+++ ChronoPiller/Controllers/HomeController.cs
@@ -1,33 +1,29 @@
 ﻿using System;
-using System.Data.Entity.Validation;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using ChronoPiller.DAL;
 using ChronoPiller.Models;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
-using Owin.Security.Providers.Orcid.Message;
 
 namespace ChronoPiller.Controllers
 {
     public class HomeController : Controller
     {
 
-        internal ChronoUser _currentUser;
+        private ChronoUser _currentUser;
 
         [Authorize]
         public ActionResult Index()
         {
-
             _currentUser = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ChronoUserManager>()
                 .FindById(Convert.ToInt32(System.Web.HttpContext.Current.User.Identity.GetUserId()));
-            using (var db = new ChronoDbContext()) { 
-            _currentUser.Prescriptions = db.Prescriptions.Where(x => x.)
-
-            return View(user);
+            using (var db = new ChronoDbContext())
+            {
+                _currentUser.Prescriptions = db.Prescriptions.Where(x => x.UserId == _currentUser.Id).ToList();
+            }
+            return View(_currentUser);
         }
     }
 }
