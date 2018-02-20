6167ee7a88c5c75ac5005a7b2a0f9da7f439edb5
Lukasz Bielenin <l.bielenin@gmail.com>
2018-02-20 15:09:47 +0100

    Create class for pills managemnet

---
+++ ChronoPiller/Models/Dose.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChronoPiller.Models
+{
+    public class Dose
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("MedicineBox")]
+        public int MedicineId { get; set; }
+        [Required]
+        public int Pills { get; set; }
+
+    }
+}
