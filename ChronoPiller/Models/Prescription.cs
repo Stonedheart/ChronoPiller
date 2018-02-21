0538d7ea924201f10230bc4acaf0eefea40bcba4
Lukasz Bielenin <l.bielenin@gmail.com>
2018-02-21 15:27:19 +0100

    Add list initialization to Prescription class

--- ChronoPiller/Models/Prescription.cs
+++ ChronoPiller/Models/Prescription.cs
@@ -10,26 +10,34 @@ namespace ChronoPiller.Models
     {
         [Key]
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
         public DateTime DateOfIssue { get; set; }
+
         [ForeignKey("User")]
         public int UserId { get; set; }
+
         public ChronoUser User { get; set; }
+
         [NotMapped]
         public List<PrescriptedMedicine> PrescriptedMedicines { get; set; }
+
         [NotMapped]
         public List<Dose> Doses { get; set; }
 
         public Prescription()
         {
+            Doses = new List<Dose>();
         }
 
         public Prescription(string name, DateTime dateOfIssue)
         {
             Name = name;
             DateOfIssue = dateOfIssue;
+            Doses = new List<Dose>();
         }
     }
 }
