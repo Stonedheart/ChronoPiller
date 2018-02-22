89083b44c0623a823a2bc66643320994e0c3ec13
Stonedheart <tytomaszto@gmail.com>
2018-02-22 12:49:18 +0100

    Add new method in DbService

--- ChronoPiller/Database/DbService.cs
+++ ChronoPiller/Database/DbService.cs
@@ -1,4 +1,7 @@
-﻿using System.Web;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ChronoPiller.DAL;
 using ChronoPiller.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
@@ -18,7 +21,17 @@ namespace ChronoPiller.Database
         {
             var context = HttpContext.Current;
             var id = context.User.Identity.GetUserId();
-            return context.GetOwinContext().GetUserManager<ChronoUserManager>().FindById(int.Parse(id));
+            var user = context.GetOwinContext().GetUserManager<ChronoUserManager>().FindById(int.Parse(id));
+            user.Prescriptions = GetPrescriptions(user.Id);
+            return user;
+        }
+
+        public List<Prescription> GetPrescriptions(int id)
+        {
+            using (var db = new ChronoDbContext())
+            {
+                return db.Prescriptions.Where(x => x.UserId == id).ToList();
+            }
         }
     }
 }
