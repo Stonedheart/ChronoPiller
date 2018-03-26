7e327693dd2d777dcfe3dcff1967de6cd7fd6a54
Łukasz Bielenin <l.bielenin@gmail.com>
2018-03-26 23:05:28 +0200

    Add js script for prescription added message

--- ChronoPiller/Scripts/NotificationJS/RequestNotification.js
+++ ChronoPiller/Scripts/NotificationJS/RequestNotification.js
@@ -7,6 +7,11 @@ function checkForPermission() {
     $(function() {
         if (Notification.permission === 'granted') {
             console.log('granted!');
+            if (sessionStorage.getItem("popUp") !== null) {
+                options.body = "That is great!";
+                let note = new Notification(sessionStorage.getItem("popUp"), options);
+                sessionStorage.clear();
+            }
 
         } else {
 
