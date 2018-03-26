cf8703a374e2d3b70201c52d1aa5715a12b7fd06
Łukasz Bielenin <l.bielenin@gmail.com>
2018-03-26 23:01:41 +0200

    Comment out temporary unused add method

--- ChronoPiller/Controllers/PrescriptionController.cs
+++ ChronoPiller/Controllers/PrescriptionController.cs
@@ -23,34 +23,238 @@ namespace ChronoPiller.Controllers
             return View();
         }
 
+//        [HttpPost]
+//        [Authorize]
+//        public ActionResult Add(FormCollection form)
+//        {
+//            try
+//            {
+//                var name = form["name"] ?? "Prescription: " + DateTime.Today;
+//                var dateOfIssue = form["dateOfIssue"] ?? DateTime.Today.ToString();
+//                var prescription = new Prescription(name, DateTime.Parse(dateOfIssue).Date);
+//                var user = DbService.User;
+//
+//                prescription.UserId = user.Id;
+//
+//                user.Prescriptions.Add(prescription);
+//
+//                DbService.SavePrescriptionToDb(prescription);
+//
+//                BackgroundJob.Enqueue(() => NotificationController.SendConfirmation(user.Email, prescription));
+//                var prescriptionId = DbService.GetPrescriptionId(prescription);
+//
+//                return RedirectToAction("Add", "Medicine", new {id = prescriptionId});
+//            }
+//            catch (Exception e)
+//            {
+//                ViewBag.ErrorMessage = e.Message;
+//                return View();
+//            }
+//        }
+
         [HttpPost]
-        [Authorize]
-        public ActionResult Add(FormCollection form)
+        public async Task<JsonResult> AddPrescriptionAsync(string json)
         {
-            var Db = new DbService();
+            var dict = new Dictionary<string, string> {{"message", "success"}};
+            if (json == null)
+            {
+                throw new ArgumentNullException();
+            }
+
             try
             {
-                var name = form["name"] ?? "Prescription: " + DateTime.Today;
-                var dateOfIssue = form["dateOfIssue"] ?? DateTime.Today.ToString();
-                var prescription = new Prescription(name, DateTime.Parse(dateOfIssue).Date);
-                var user = Db.User;
+                var result = AddAsync(json);
+                await result;
 
-                prescription.UserId = user.Id;
-
-                user.Prescriptions.Add(prescription);
-
-                Db.SavePrescriptionToDb(prescription);
-
-                BackgroundJob.Enqueue(() => NotificationController.SendConfirmation(user.Email, prescription));
-                var prescriptionId = Db.GetPrescriptionId(prescription);
-
-                return RedirectToAction("Add", "Medicine", new {id = prescriptionId});
+                return Json(dict);
             }
             catch (Exception e)
             {
-                ViewBag.ErrorMessage = e.Message;
-                return View();
+                ModelState.AddModelError("", e.Message);
+                dict["message"] = e.Message;
+                return Json(dict);
             }
+        }
+
+        private async Task AddAsync(string json)
+        {
+            var jsonObject = JObject.Parse(json);
+            var prescriptionData = jsonObject["prescription"];
+            var medicinesData = jsonObject["medicines"].ToList();
+
+            var context = new ChronoDbContext();
+
+            var prescriptionId = await SavePrescriptionFromJsonAsync(prescriptionData["name"].ToString(),
+                prescriptionData["dateOfIssue"].ToString(), context);
+
+            var medicineIds = new List<int>();
+            medicinesData.ForEach(async x =>
+                medicineIds.Add(await SaveMedicineFromJsonAsync(x["name"].ToString(), context)));
+
+
+            for (var i = 0; i < medicineIds.Count; i++)
+            {
+                var medicineBoxId = await SaveMedicineBoxFromJsonAsync(medicineIds[i],
+                    medicinesData[i]["medicineBoxCapacity"].ToString(),
+                    medicinesData[i]["activeSubstanceAmountInMg"].ToString(), context);
+                await SavePrescriptedMedicineFromJsonAsync(medicinesData[i]["name"].ToString(),
+                    medicinesData[i]["startUsageDate"].ToString(),
+                    medicinesData[i]["prescriptedBoxCount"].ToString(),
+                    medicinesData[i]["dose"].ToString(),
+                    prescriptionId,
+                    medicineBoxId, context);
+            }
+            ;
+
+            context.Dispose();
+        }
+
+        private async Task<int> SavePrescriptionFromJsonAsync(string name, string dateOfIssue, ChronoDbContext context)
+        {
+            if (name.IsNullOrEmpty())
+            {
+                throw new NullNameException("Prescription has to have a name!");
+            }
+
+            DateTime date;
+            try
+            {
+                date = DateTime.Parse(dateOfIssue);
+            }
+            catch (Exception)
+            {
+                throw new InvalidDateException("Prescription date is invalid!");
+            }
+            var userId = int.Parse(User.Identity.GetUserId());
+            var prescription = new Prescription(userId, name, date);
+            try
+            {
+                context.Prescriptions.Add(prescription);
+                context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                using (var writer = new StreamWriter("C:\\jsws\\entityLog.txt"))
+                {
+                    writer.Write(e);
+                }
+            }
+
+            return prescription.Id;
+        }
+
+
+        private async Task<int> SaveMedicineFromJsonAsync(string name, ChronoDbContext context)
+        {
+            if (name.IsNullOrEmpty())
+            {
+                throw new NullNameException("Medicine name cannot be null!");
+            }
+            var medicine =
+                new Medicine(name);
+            context.Medicines.Add(medicine);
+            context.SaveChanges();
+
+            var id = medicine.Id;
+
+            return id;
+        }
+
+        private async Task<int> SaveMedicineBoxFromJsonAsync(int medicineId, string capacity,
+            string activeSubstanceAmountInMg, ChronoDbContext context)
+        {
+            int boxCapacity;
+            try
+            {
+                boxCapacity = int.Parse(capacity);
+            }
+            catch (ArgumentNullException ex)
+            {
+                ex = new ArgumentNullException("Box's capacity can't be null!");
+
+                throw ex;
+            }
+            float boxActiveSubstance;
+            try
+            {
+                boxActiveSubstance = float.Parse(activeSubstanceAmountInMg);
+            }
+            catch (ArgumentNullException ex)
+            {
+                ex = new ArgumentNullException("Active substance can't be null!");
+
+                throw ex;
+            }
+            var medicineBox = new MedicineBox(medicineId, boxCapacity, boxActiveSubstance);
+            context.MedicineBoxes.Add(medicineBox);
+
+            var id = medicineBox.Id;
+
+            return id;
+        }
+
+        private async Task SavePrescriptedMedicineFromJsonAsync(string name, string startUsageDate,
+            string prescriptedBoxCount, string dose, int prescriptionId, int medicineBoxId, ChronoDbContext context)
+        {
+            if (name.IsNullOrEmpty())
+            {
+                throw new NullNameException("Name cannot be empty!");
+            }
+            DateTime medicineUsageDate;
+            try
+            {
+                medicineUsageDate = DateTime.Parse(startUsageDate).Date;
+            }
+            catch (ArgumentNullException ex)
+            {
+                ex = new ArgumentNullException("Start usage date of medicine cannot be null!");
+                throw ex;
+            }
+            catch (FormatException format)
+            {
+                format = new FormatException("Start usage date of med has ivalid format!");
+                throw format;
+            }
+            int boxCount;
+            try
+            {
+                boxCount = int.Parse(prescriptedBoxCount);
+            }
+            catch (ArgumentNullException ex)
+            {
+                ex = new ArgumentNullException("Prescripted box count can't be empty!");
+                throw ex;
+            }
+            catch (FormatException format)
+            {
+                format = new FormatException("Invalid box count format!");
+                throw format;
+            }
+
+            int prescriptedDose;
+            try
+            {
+                prescriptedDose = int.Parse(dose);
+            }
+            catch (ArgumentNullException ex)
+            {
+                ex = new ArgumentNullException("Dose can't be empty!");
+                throw ex;
+            }
+            catch (FormatException format)
+            {
+                format = new FormatException("Invalid dose format!");
+                throw format;
+            }
+
+            var prescriptedMedicine = new PrescriptedMedicine(name,
+                medicineUsageDate,
+                boxCount,
+                prescriptedDose,
+                prescriptionId,
+                medicineBoxId);
+            context.PrescriptedMedicines.Add(prescriptedMedicine);
+            context.SaveChanges();
         }
 
         [HttpGet]
