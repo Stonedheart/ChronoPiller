4b3a4782246c803865bcb4257fc237e7e9acb93b
Stonedheart <tytomaszto@gmail.com>
2018-04-04 14:24:20 +0200

    Add enum for notification types

---
+++ ChronoPiller/Models/NotificationTypeEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace ChronoPiller.Models
+{
+    public enum NotificationTypeEnum
+    {
+        Dose = 1,
+        OutOfPills = 2,
+        BeforeMeal = 3,
+        AfterMeal = 4,
+        PrescriptionExpiration = 5
+    }
+}
