1f599d22bbbc7d2cd338984bd44d0e93a9ca2436
Stonedheart <tytomaszto@gmail.com>
2018-04-04 13:43:32 +0200

    Add Notification class

---
+++ ChronoPiller/Models/Notification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChronoPiller.Models
+{
+    public class Notification
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public ChronoUser User { get; set; }
+        [ForeignKey("Prescription")]
+        public int PrescriptionId { get; set; }
+        public Prescription Prescription { get; set; }
+        [ForeignKey("NotificationType")]
+        public int NotificationTypeId { get; set; }
+        public string HangfireId { get; set; }
+        public string Cron { get; set; }
+        public DateTime NextExecution { get; set; }
+    }
+}
